070783c3d25c00794026f75ae737e8355c6a29e0
gabrielmds222 <gabrielmedsilva@outlook.com>
2023-07-20 19:39:11 -0300

    feat: Componente de cards e ajuste no header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,19 +5,9 @@ import NovaTransacao from "../NovaTransacao";
 
 const Header = () => {
   return (
-    <header className="bg-indigo-600">
-      <div className="mx-auto px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
-        <div className="sm:flex sm:items-center sm:justify-between">
-          <div className="text-center sm:text-left">
-            <div className="bg-slate-100 p-2 rounded-full">
-              <User size={32} color="#000" weight="bold" />
-            </div>
-          </div>
-
-          <div className="mt-4 flex flex-col gap-4 sm:mt-0 sm:flex-row sm:items-center">
-            <NovaTransacao />
-          </div>
-        </div>
+    <header className="bg-indigo-600 h-48 text-center">
+      <div className="pt-10 text-white">
+        <h1 className="text-4xl">Controle Financeiro</h1>
       </div>
     </header>
   );
