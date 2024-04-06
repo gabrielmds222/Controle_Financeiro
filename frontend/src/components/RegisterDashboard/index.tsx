68ceebbac1dd8100c2b45204d9a087e1dd722dae
gabrielmedsilva <gabrielmedsilva@outlook.com>
2024-04-06 20:51:01 -0300

    fix: Resolve table error

--- frontend/src/components/RegisterDashboard/index.tsx
+++ frontend/src/components/RegisterDashboard/index.tsx
@@ -1,3 +1,5 @@
+import { Trash } from "@phosphor-icons/react";
+
 function RegisterDashboard() {
   return (
     <tbody className="text-center divide-y divide-gray-200">
@@ -17,47 +19,7 @@ function RegisterDashboard() {
             href="#"
             className="inline-block rounded bg-indigo-600 px-4 py-2 text-xs font-medium text-white hover:bg-indigo-700"
           >
-            View
-          </a>
-        </td>
-      </tr>
-
-      <tr>
-        <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-          Jane Doe
-        </td>
-        <td className="whitespace-nowrap px-4 py-2 text-gray-700">
-          04/11/1980
-        </td>
-        <td className="whitespace-nowrap px-4 py-2 text-gray-700">
-          Web Designer
-        </td>
-        <td className="whitespace-nowrap px-4 py-2 text-gray-700">$100,000</td>
-        <td className="whitespace-nowrap px-4 py-2">
-          <a
-            href="#"
-            className="inline-block rounded bg-indigo-600 px-4 py-2 text-xs font-medium text-white hover:bg-indigo-700"
-          >
-            View
-          </a>
-        </td>
-      </tr>
-
-      <tr>
-        <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-          Gary Barlow
-        </td>
-        <td className="whitespace-nowrap px-4 py-2 text-gray-700">
-          24/05/1995
-        </td>
-        <td className="whitespace-nowrap px-4 py-2 text-gray-700">Singer</td>
-        <td className="whitespace-nowrap px-4 py-2 text-gray-700">$20,000</td>
-        <td className="whitespace-nowrap px-4 py-2">
-          <a
-            href="#"
-            className="inline-block rounded bg-indigo-600 px-4 py-2 text-xs font-medium text-white hover:bg-indigo-700"
-          >
-            View
+            <Trash size={20} color="#f5f0f0" />
           </a>
         </td>
       </tr>
